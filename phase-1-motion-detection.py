a743cd286826660311fc0a0b62f5c2ca21511e7e
Jacob Rodriguez <jacobrdz47@gmail.com>
2025-08-02 05:17:18 +0100

    feat: Integrate Picamera2 and image capture on motion detected.
    - PIR waits until motion is detected, then it turns on camera and saved the image to the images directory

--- phase-1-motion-detection.py
+++ phase-1-motion-detection.py
@@ -1,23 +1,45 @@
 from gpiozero import MotionSensor
+from picamera2 import Picamera2
 import time
+import datetime
+import os
 
-def on_motion(e):
-    print(f"MOOVING: is Active? {e.is_active}")
-    
-def on_no_motion(e):
-    print(f"Is Active: {e.is_active}")
-
-# GPIO 4 (PIN 7)
+# PIR - GPIO 4 (PIN 7)
 pir = MotionSensor(4)
-pir.when_no_motion = on_no_motion
-pir.when_motion = on_motion
 
+# PiCamera
+camera = Picamera2()
+camera.preview_configuration.main.size = (1280, 1280)
+camera.preview_configuration.align()
+capture_config = camera.create_still_configuration()
+camera.configure("preview")
 
-try:
-    while True:
-        # print("Waiting.")
-        time.sleep(0.1)
-except KeyboardInterrupt:
-    print("Stopping...")
-    
+def create_file_name():
+    images_directory = "images"
+    # Creates the images directory if it's not already there
+    os.makedirs(images_directory, exist_ok=True)
+    file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S.jpg")
+
+    return os.path.join(images_directory, file_name)
+
+def on_motion():
+    print("Motion detected!!!")
+    camera.start(show_preview=True)
+    image_name = create_file_name()
+    time.sleep(.2)
+    camera.switch_mode_and_capture_file(capture_config, image_name)
+    camera.stop_preview()
+    print("Saved image!")
+    time.sleep(5)
+
+def main():
+    try:
+        while True:
+            print("Waiting for motion...")
+            pir.wait_for_motion()
+            on_motion()
+    except KeyboardInterrupt:
+        print("Stopping...")
+
+main()
 
