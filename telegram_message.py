7fc1d4ce0acdbbab99766a3b018237b6846a9c66
Jacob Rodriguez <jacobrdz47@gmail.com>
2025-08-04 15:16:18 +0100

    Telegram bot: made the CatSecurityBot the default way to interact with the bot
    - Add innit, example hello command, start & stop polling, and add handlers functions in class

--- telegram_message.py
+++ telegram_message.py
@@ -1,45 +1,53 @@
 import dotenv
 import os
+import logging
+
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import logging
 
+class CatSecurityBot:
+    "CatSecurityBot is a Telegram bot that sends messages/photos to the user (defined by the MESSAGE_ID in .env)."
 
-load_dotenv()
-BOT_KEY = os.getenv("BOT_KEY")
-MESSAGE_ID = os.getenv("MESSAGE_ID")
-
-class Bot:
-    def __init__(self, debug_enabled=True):
-        self.app = ApplicationBuilder().token(BOT_KEY).build()
-
-        hello_handler = CommandHandler("hello", hello)
-        self.app.add_handler(hello_handler)
-        self.app.run_polling()
+    def __init__(self, debug_enabled: bool = True,):
+        """Initializes with bot key and user ID from environment variables. By default, it allows for debug logging."""
+        load_dotenv()
+        BOT_KEY = os.getenv("BOT_KEY")
+        self.message_id = os.getenv("MESSAGE_ID")
+        if not BOT_KEY or not self.message_id:
+            raise ValueError("No Bot Key or Message ID found.")
 
         if debug_enabled:
             logging.basicConfig(
                 format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                level=logging.INFO
-            )
+                level=logging.INFO)
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+        self.app = ApplicationBuilder().token(BOT_KEY).build()
+        self.add_handlers()
+        self.start_polling()
+
+    def start_polling(self):
+        try:
+            print("Bot started polling...")
+            self.app.run_polling()
+        except Exception as e:
+            print(f"Error starting polling: Exception Type({type(e).__name__}) {e}")
+        finally:
+            print("Stopped polling.")
+
+    def add_handlers(self):
+        """Adds all of the commands to the bot."""
+        # Todo:
+        # - Add send_cat_detected
+        # - Add send_unkown_detected
+        self.app.add_handler(CommandHandler("hello", self.hello_command))
+
+    async def hello_command(self, update: Update, ctx: ContextTypes.DEFAULT_TYPE) -> None:
+        await update.message.reply_text(f"Hello {update.effective_user.first_name}")
+
 
 def main():
-    print("Starting...")
-    logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO
-    )
-    app = ApplicationBuilder().token(BOT_KEY).build()
-
-    hello_handler = CommandHandler("hello", hello)
-    app.add_handler(hello_handler)
-    
-    app.run_polling()
-
+    bot = CatSecurityBot()
 
 if __name__ == "__main__":
     main()
