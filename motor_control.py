d969a141c79dd271955037d593f5f01b06269e6e
Jacob Rodriguez <jacobrdz47@gmail.com>
2025-08-05 06:03:13 +0100

    Add the proper PWM field in Motor init and add cleanup func for graceful shutdown

--- motor_control.py
+++ motor_control.py
@@ -1,5 +1,6 @@
 import gpiozero import Motor, PWMOutputDevice
 import time
+import signal
 
 MOTOR_1 = 23 #GPIO 23
 MOTOR_2= 24 #GPIO 24
@@ -9,20 +10,35 @@ class DispenseMotor:
     """Used to rotate a auger screw to dispense food. """
     
     def __init__(self):
-        self.motor = Motor(forward=MOTOR_1, backward=MOTOR_2, pwm=True)
-        self.pwm_control = PWMOutputDevice(pin=PWM_PIN)
+        self.motor = Motor(forward=MOTOR_1, backward=MOTOR_2, enable=PWM_PIN)
 
     def dispense_food(self):
-        self.motor.forward(0.3)
-        time.sleep(4)
-        self.motor.backward(0.3)
-        time.sleep(1)
-        self.motor.stop()
+        try:
+            print("Running motor forward...")
+            self.motor.forward(0.3)
+            time.sleep(4)
 
+
+            print("Running motor backwards...")
+            self.motor.backward(0.3)
+            time.sleep(1)
+
+            print("Stopping motor...")
+            self.motor.stop()
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            self.cleanup()
+
+    def cleanup(self):
+        self.motor.stop()
+        print("Motor stopped cleanly.")
+        exit(0)
 
 def main():
-    motor = DispenseMotor()
-    motor.dispense_food()
+    try:
+        motor = DispenseMotor()
+        motor.dispense_food()
 
 if __name__ == "__main__":
     main()
